a603366d3c2f8093d91a7d9663025078c2a3c4dd
Lý Thái Bảo <baoly069@gmail.com>
2025-06-20 18:09:31 +0700

    upload project

---
+++ asset/js/main.js
@@ -0,0 +1,74 @@
+let formInput = document.querySelectorAll(".form__input input");
+let formInputName = document.querySelector(".form__userName input");
+let formInputMail = document.querySelector(".form__mail input");
+let formInputPassword = document.querySelector(".form__password input");
+let formInputConfirm = document.querySelector(".form__confirm input");
+let form = document.querySelector(".form");
+let clearAll = document.querySelector(".clearAll");
+
+function showError(itemError, message) {
+  itemError.nextElementSibling.textContent = message;
+}
+
+function showSuccess(itemSuccess) {
+  showError(itemSuccess, "");
+  itemSuccess.classList.add("success");
+}
+
+function isLengthError(input, min, max) {
+  if (input.value.trim().length < min && input.value.trim() != "") {
+    showError(input, `Phai lon hon ${min} ki tu`);
+    return true;
+  } else if (input.value.length > max) {
+    showError(input, `Phai be hon ${max} ki tu`);
+    return true;
+  } else if (!input.value) {
+    showError(input, "Khong duoc bo trong");
+    return true;
+  } else {
+    showSuccess(input);
+    return false;
+  }
+}
+
+function isErrorMail(input) {
+  const regex =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  if (isLengthError(input)) {
+    showError(input, "Khong duoc bo trong");
+    return true;
+  }
+  if (regex.test(input.value.trim())) {
+    showSuccess(input);
+    return false;
+  } else {
+    showError(input, "Email khong hop le");
+    return true;
+  }
+}
+
+function isErrorPasswordMatch(password, confirmPassword) {
+  if (password.value != confirmPassword.value) {
+    showError(confirmPassword, "Mat khau khong trung khop");
+    return true;
+  } else {
+    showSuccess(confirmPassword);
+    return false;
+  }
+}
+clearAll.addEventListener("click", (e) => {
+  form.reset();
+  formInput.forEach((value, index) => {
+    showError(value, "");
+    value.classList.remove("success");
+  });
+});
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  isLengthError(formInputName, 5, 50);
+  isLengthError(formInputPassword, 5, 50);
+  isLengthError(formInputMail, 5, 50);
+  isErrorMail(formInputMail);
+  isErrorPasswordMatch(formInputPassword, formInputConfirm);
+});
+console.log(formInputName);
